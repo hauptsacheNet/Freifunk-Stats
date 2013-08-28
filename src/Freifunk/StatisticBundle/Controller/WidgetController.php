989837639ce16e19ac10eb2376394fdc110ef1de
ferdynator <byf.ferdy@googlemail.com>
2013-08-28 12:01:57 +0200

    First basic widget returns all clients for a node.

--- src/Freifunk/StatisticBundle/Controller/WidgetController.php
+++ src/Freifunk/StatisticBundle/Controller/WidgetController.php
@@ -5,6 +5,7 @@ namespace Freifunk\StatisticBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class DefaultController
@@ -18,10 +19,36 @@ class WidgetController extends Controller
      * @Route("/widget")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
 
+        $params = $request->query->all();
 
-        return array();
+        if (!$params['node']) {
+            return array();
+        }
+
+        $manager = $this->getDoctrine()->getManager();
+        $node_repository = $manager->getRepository('FreifunkStatisticBundle:Node');
+        $stat_repository = $manager->getRepository('FreifunkStatisticBundle:NodeStat');
+
+        $nodes = (is_string($params['node'])) ? array($params['node']) : $params['node'];
+
+        $clients = 0;
+        foreach ($nodes as $mac) {
+
+            $node = $node_repository->findByMac($mac);
+
+            if ($node) {
+                $stats = $stat_repository->getLastStatOf($node);
+                $clients += $stats->getClientCount();
+            }
+
+        }
+
+        return array(
+            'nodes' => $nodes,
+            'clients' => $clients
+        );
     }
 }

f5eabe13d60ebd500febaebee3e029834205613c
Marco Pfeiffer <cheat2000-git@yahoo.de>
2013-08-29 11:27:21 +0200

    js controller und widget nachlade js erstellt

---
+++ src/Freifunk/StatisticBundle/Controller/JsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Freifunk\StatisticBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class JsController
+ * @package Freifunk\StatisticBundle\Controller
+ *
+ * @Route("/js")
+ */
+class JsController extends Controller
+{
+    /**
+     * @Route("/widget")
+     */
+    public function widgetJsAction(Request $request)
+    {
+        $response = $this->render("FreifunkStatisticBundle:Js:widget.js.twig", array(
+            'baseurl' => $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath()
+        ));
+        $response->headers->add(array(
+            'Content-Type' => 'text/javascript; charset=UTF-8'
+        ));
+        // cache
+        $response->setPublic();
+        $response->setETag(md5($response->getContent()));
+        $response->isNotModified($this->getRequest());
+        $response->setMaxAge(60 * 60 * 24 * 30);
+
+        return $response;
+    }
+}
---
+++ src/Freifunk/StatisticBundle/Tests/Controller/JsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Freifunk\StatisticBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class JsControllerTest extends WebTestCase
+{
+}

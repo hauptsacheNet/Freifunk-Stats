e5799f26fb3b7f1f9231f6149e638f93cedc2e71
Marco Pfeiffer <cheat2000-git@yahoo.de>
2013-08-30 12:16:32 +0200

    widget gestyled

--- src/Freifunk/StatisticBundle/Controller/WidgetController.php
+++ src/Freifunk/StatisticBundle/Controller/WidgetController.php
@@ -18,12 +18,43 @@ use Ob\HighchartsBundle\Highcharts\Highchart;
 class WidgetController extends Controller
 {
 
+    private function createHighChart($xTitle, $yTitle, $accent = '#E0266B', $text = '#000')
+    {
+        $chart = new Highchart();
+        $chart->title->text(null);
+        $chart->chart->backgroundColor('transparent');
+        $chart->chart->style(array('color' => $accent));
+        $chart->title->style(array('color' => $accent));
+        $chart->xAxis->labels(array('style' => array('color' => $text)));
+        $chart->xAxis->title(array('text' => $xTitle, 'style' => array('color' => $text)));
+        $chart->xAxis->lineColor($accent);
+        $chart->xAxis->tickColor($accent);
+        $chart->yAxis->labels(array('style' => array('color' => $text)));
+        $chart->yAxis->title(array('text' => $yTitle, 'style' => array('color' => $text)));
+        $chart->yAxis->lineColor($accent);
+        $chart->yAxis->tickColor($accent);
+        $chart->colors = array(
+            $accent,
+            '#2f7ed8',
+            '#0d233a',
+            '#8bbc21',
+            '#910000',
+            '#1aadce',
+            '#492970',
+            '#f28f43',
+            '#77a1e5',
+            '#c42525',
+            '#a6c96a'
+        );
+        return $chart;
+    }
+
     /**
      * First Widget, very basic. Just displays the number of clients per node.
      * Example request: `/test/div?node=<nodeName>
      *
-     * @param Request  $request
-     * @param string   $id
+     * @param Request $request
+     * @param string $id
      *
      * @return array
      *
@@ -62,8 +93,8 @@ class WidgetController extends Controller
      * Returns a nice graph that displays all Clients for
      * the nodes over the selected period of time.
      *
-     * @param Request  $request
-     * @param int      $id
+     * @param Request $request
+     * @param int $id
      *
      * @return array
      *
@@ -114,14 +145,22 @@ class WidgetController extends Controller
             }
         }
 
-        $ob = new Highchart();
+        $ob = $this->createHighChart(null, 'Anzahl Clients');
         $ob->chart->renderTo($id . '_chart');
         $ob->chart->type('line');
-
-        $ob->title->text(null);
-        $ob->xAxis->title(array('text'  => "Zeitspanne"));
+        $ob->chart->spacingRight(130);
         $ob->xAxis->type('datetime');
-        $ob->yAxis->title(array('text'  => "Anzahl von Clients"));
+        $ob->legend->align('right');
+        $ob->legend->verticalAlign('top');
+        $ob->legend->x(125);
+        $ob->legend->y(30);
+        $ob->legend->layout('vertical');
+        $ob->legend->floating(true);
+        $ob->legend->itemWidth(120);
+        $ob->legend->borderWidth(0);
+        $ob->labels->style(array(
+            'font-weight' => 'bold'
+        ));
 
         $ob->series($series);
 
